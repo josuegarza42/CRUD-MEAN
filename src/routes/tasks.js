44e171a58b4241ce1064a6034708b0f3e0bdbc90
Josué Garza <A281876@alumnos.uaslp.mx>
2022-09-17 18:54:46 -0500

    obtener una o mas tareas de la base de datos

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -2,10 +2,20 @@ const router = require('express').Router();
 const mongojs = require('mongojs');
 const db = mongojs('mean-db', ['tasks']);
 
+//get all tasks from database
 router.get('/tasks', (req, res, next) => {
     db.tasks.find((err, tasks) => {
-        if(err) return next(err);
+        if (err) return next(err);
         res.json(tasks);
+    });
+});
+
+//get single tasks from database
+
+router.get('/tasks/:id', (req, res, next) => {
+    db.tasks.findOne({ _id: (req.params.id) }, (err, task) => {
+        if (err) return next(err);
+        res.json(task);
     });
 });
 
