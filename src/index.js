ab6ccb5a74b16ca2358420ce7733ae2f9c073950
Josué Garza <A281876@alumnos.uaslp.mx>
2022-09-17 17:42:01 -0500

    primeras configuraciones al server de express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+
+//setup express
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+  console.log('Server is running on port 3000', app.get('port'));
+});
+
